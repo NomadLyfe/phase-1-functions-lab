3cef1c81b94fb505885e12b95a97120b2ca74cd4
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-04 09:27:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+    const distanceBlocks = Math.abs(42 - destination);
+    return distanceBlocks;
+};
+
+function distanceFromHqInFeet(destination) {
+    const distanceFeet = Math.abs(42 - destination)*264;
+    return distanceFeet;
+};
+
+function distanceTravelledInFeet(start, destination) {
+    const distanceTravelled = Math.abs(start - destination)*264;
+    return distanceTravelled;
+};
+
+function calculatesFarePrice(start, destination) {
+    const distanceTravelled = Math.abs(start - destination)*264;
+    if (distanceTravelled <= 400) {
+        return 0;
+    } else if (distanceTravelled <= 2000) {
+        return 0.02 * (distanceTravelled - 400);
+    } else if (distanceTravelled <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+};
+
+
